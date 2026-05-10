3f98e481ab2729d750705895e90ef103f475621a
Kamil quuixly <quuixly@gmail.com>
2026-05-10 14:16:42 +0200

    feat: Make MCP Client async

--- src/agent/src/mcp_client.py
+++ src/agent/src/mcp_client.py
@@ -1,33 +1,36 @@
 import asyncio
-from typing import List, Any, Dict
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
 
 class MCPClient:
-    def __init__(self, server_configs: Dict[str, Dict[str, str]]):
+    def __init__(self, server_configs):
         self.client = MultiServerMCPClient(server_configs)
-        self.tools: List[Any] = []
+        self.tools = []
 
     async def fetch_tools(self):
         self.tools = await self.client.get_tools()
 
         return self.tools
 
-    async def execute_tool(self, name: str, args: Dict):
+    async def execute_tool(self, name, args):
         tool = next((t for t in self.tools if t.name == name), None)
-
         if not tool:
             raise ValueError(f"Tool {name} does not exist.")
-
         return await tool.ainvoke(args)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
 if __name__ == "__main__":
     async def main():
         configs = {
-            "weather_service": {
+            "Environment": {
                 "transport": "http",
-                "url": "http://localhost:8000/mcp"
+                "url": "http://127.0.0.1:8000/mcp"
             }
         }
 
